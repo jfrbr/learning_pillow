ff55e655eede859fa2639434b548ae2a2c2cd3b8
juan <juan.franca@gmail.com>
2022-01-23 22:30:13 +0100

    Adding new features

--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageDraw, ImageFont
 import os
 
 def main():
 
-#    cwd = os.getcwd()
-#    print(cwd)
-
     im = Image.open("../assets/images/yellow-italian-car.jpg")
+    
     print(im.format, im.size, im.mode)
 
     out = im.filter(ImageFilter.DETAIL)
@@ -15,6 +13,23 @@ def main():
     box = (100, 100, 400, 400)
     region = im.crop(box)
     region.save("../assets/images/yellow-italian-car-cropped.jpg")
+
+    blurred = im.filter(ImageFilter.BLUR)
+    blurred.save("../assets/images/yellow-italian-car-blurred.jpg")
+
+    idraw = ImageDraw.Draw(im)
+    text = "Put your text here"
+
+    #font = ImageFont.truetype("arial.ttf", size=18)
+    idraw.text((im.size[0]/2,im.size[1]/2), text)
+
+    im.save("../assets/images/yellow-italian-car-watermark.jpg")
+
+
+    grayscale = im.convert('L')
+    grayscale.save("../assets/images/yellow-italian-car-grayscale.jpg")
+
+
 
 
 if __name__ == "__main__":
